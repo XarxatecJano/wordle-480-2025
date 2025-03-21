52e2400c063c7fa703f48cfc1c04c8d6f258fefe
GuillermoGomezGascon <willgomezgascon@gmail.com>
2025-03-21 10:52:24 +0100

    refactor: crear la implementación de ILetterDisplay

---
+++ src/LetterDisplayImpl.ts
@@ -0,0 +1,34 @@
+import { ILetterDisplay } from "./ILetterDisplay";
+import { IDOMAccess } from "./IDOMAccess";
+
+export class LetterDisplayImpl implements ILetterDisplay {
+    private domAccess: IDOMAccess;
+
+    constructor(domAcces: IDOMAccess) {
+        this.domAccess = domAcces;
+    }
+
+    private getCellElement(turn: number, position: number): HTMLElement | null {
+        const row = this.domAccess.getELementById(`row_${turn}`);
+        if (row) {
+            const cells = Array.from(row.children);
+            return cells[position] as HTMLElement || null;
+        }
+        return null;
+    }
+
+    setLetter(turn: number, position: number, letter: string): void {
+        const cell = this.getCellElement(turn, position);
+        if (cell) {
+            cell.textContent = letter
+        }
+    }
+    
+    clearLetter(turn: number, position: number): void {
+        const cell = this.getCellElement(turn, position);
+        if (cell) {
+            cell.textContent = ""
+        }
+    }
+
+}
