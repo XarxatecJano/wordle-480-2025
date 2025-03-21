f66601fd25949be3f9222bba6a3209eb14119cdf
GuillermoGomezGascon <willgomezgascon@gmail.com>
2025-03-21 13:08:05 +0100

    refactor: crear clase InputHandler

---
+++ src/InputHandler.ts
@@ -0,0 +1,31 @@
+import { IInputHandler } from "./IINputHandler";
+import { MAX_WORD_SIZE } from "./env";
+
+export class InputHandle implements IInputHandler {
+    private validLetterCodes: string[];
+    private currentPosition: number;
+
+    constructor(validLetterCodes: string[], currentPosition: number) {
+        this.validLetterCodes = validLetterCodes;
+        this.currentPosition = currentPosition;
+    }
+
+    setCurrentPosition(position: number): void {
+        this.currentPosition = position;
+    }
+
+    isValidLetter(code: string): boolean {
+        return this.validLetterCodes.includes(code) && this.currentPosition < MAX_WORD_SIZE
+    }
+    isEnterKey(code: String): boolean {
+        return code === "Enter"
+    }
+    isBackspaceKey(code: String): boolean {
+        return code === "Backspace"
+    }
+    transformCodeToLetter(code: string): string {
+        if (code === ";") return "Ñ";
+        return code.split("y")[1];
+    }
+
+}
